e467fce31b405f155ff8ae9bca2fb37ee3036ab9
Andres Gurpide Lasheras <andres.gurpide@gmail.com>
2021-04-22 10:22:02 +0200

    Removed the cor factor since we know normalize by the total flux

--- marxsim.py
+++ marxsim.py
@@ -1,8 +1,15 @@
+# @Author: Andrés Gúrpide <agurpide>
+# @Date:   21-04-2021
+# @Email:  agurpidelash@irap.omp.eu
+# @Last modified by:   agurpide
+# @Last modified time: 22-04-2021
+
+
+
 #%% initialisation
 import os
-from xspec import *
 import numpy as np
-from regions import *
+from regions import read_ds9
 from astropy.io import fits
 from astropy.time import Time
 from astropy import units as u
@@ -17,7 +24,7 @@ import matplotlib.pyplot as plt
 #Don't forget to launch this on your ciao python interpreter
 
 #saotrace might not work if you have conflicts in your perl libraries : initialising saotrace might
-#replace one (but not all) of your perl libraries environment variables. 
+#replace one (but not all) of your perl libraries environment variables.
 #You can manually setup the other variables in your saotrace starting script
 
 '''
@@ -68,7 +75,7 @@ pta=0.492
 #radius affected by pile-up in the initial data in arsecs
 # rlim_pileup=1.2
 rlim_pileup=1.0
- 
+
 '''############'''
 
 if sao_arg=='maybe':
@@ -79,12 +86,12 @@ if sao_arg=='maybe':
             lock=1
 else:
     decis=sao_arg
-    
+
 if decis=='yes':
     decis_int=1
 elif decis=='no':
     decis_int=0
-    
+
 
 os.chdir(sdir)
 
@@ -94,7 +101,7 @@ for file in os.listdir('./'):
             ditherfile=file
         if file.endswith('_evt2.fits'):
             evt2=file
-            
+
 #reading the data file, which is assumed to have the same name as the region file
 name=regfile[:regfile.find('.')]
 data_file=name+'.pi'
@@ -109,13 +116,8 @@ source_dec=str(source_reg.center.dec.deg)
 '''
 the region doesn't actually enclose the full counts of the source, so we can manually
 correct by comparison to a full region, which here has been manually evaluated
-to a 7" circle region centered on the other region's centroid for the 2950 observation
+to a 7" circle region centered on the other region's centroid for the 2950 observation --> REMOVE THIS COMMENT?
 '''
-
-if regfile=='ngc1313x1_extraction.reg':
-    cor_factor=0.94
-else:
-    cor_factor=1
 
 #reading the event file to get some more parameters
 fits_evt2=fits.open(evt2)
@@ -170,11 +172,11 @@ if instrum=='ACIS':
     elif chip_id in [4,5,6,7,8,9]:
         detector='ACIS-S'
         subsys='ACIS-S'+str(chip_id-4)
-    
+
 if instrum=='HRC':
     detector=input('Since the CCD number overlap in this instrument, please confirm'\
                    +' if the detecor used is HRC-I or HRC-S')
-        
+
 os.system('asphist infile="'+ditherfile+'" outfile="marx.asp" evtfile="'+evt2+'" clobber=yes')
 
 #here we only use mkarf to create the response file, a more accurate version can be computed with mkwarf
@@ -186,11 +188,11 @@ os.system('mkarf detsubsys="'+subsys+'" grating="'+grating+'" outfile="marx.arf"
           +'" asphistfile="marx.asp" sourcepixelx='+ctr_x+' sourcepixely='+ctr_y+' engrid="0.3:10.0:'+binning
           +'" maskfile=NONE pbkfile=NONE dafile=NONE verbose=1 mode=h clobber=yes')
 
-#not done in the marx tutorial but should be done for a point source...right ? 
+#not done in the marx tutorial but should be done for a point source...right ?
 os.system('arfcorr infile="'+evt2+'[sky=region('+regfile+')][bin sky]" region="region('+regfile+\
           ')" x='+ctr_x+' y='+ctr_y+' arf=marx.arf outfile=marx_corr.arf clobber=yes')
-    
-#if you want to create an image file to check if the detection is correctly centered    
+
+#if you want to create an image file to check if the detection is correctly centered
 # os.system('dmcopy "'+evt2+'"[EVENTS][bin x='+str(int(float(ctr_x)-100))+':'+str(int(float(ctr_x)+100))\
 #           +':1,y='+str(int(float(ctr_y)-100))+':'+str(int(float(ctr_y)+100))\
 #           +':1" image_src.fits option=image clobber=yes')
@@ -198,15 +200,15 @@ os.system('arfcorr infile="'+evt2+'[sky=region('+regfile+')][bin sky]" region="r
 #source energy computation
 os.system('dmtcalc "'+evt2+'[EVENTS][sky=region('+regfile+')]" marx_evt2_energy.fits'\
           +' expr="energy=(float)pi*0.0149" clobber=yes')
-    
+
 #bg energy computation. Should be negligible, but included nonetheless
 os.system('dmtcalc "'+evt2+'[EVENTS][sky=region('+bgregfile+')]" marx_bg_energy.fits'\
           +' expr="energy=(float)pi*0.0149" clobber=yes')
-    
+
 #net histogram computation with real energy binning
 os.system('dmextract "marx_evt2_energy.fits[bin energy=0.3:9.999:'+binning+']"'\
           +' bkg="marx_bg_energy.fits" outfile=marx_extract.spec clobber=yes opt=generic')
-    
+
 #reading the newly created files
 arf_corr=fits.open('marx_corr.arf')
 arf_data=arf_corr[1].data
@@ -218,27 +220,27 @@ expos=str(round(extract_data['BG_EXPOSURE'][0],1))
 
 '''
 Now we create the spectrum file which will serve as an argument for marx/sao
-For Marx the (received) flux column is supposed to be a normalized flux DENSITY so the net rate column needs 
-to be divided by the binning, on top of the effective area using the arf and 
+For Marx the (received) flux column is supposed to be a normalized flux DENSITY so the net rate column needs
+to be divided by the binning, on top of the effective area using the arf and
 the correction factor to account for the loss percentage.
 '''
 
 if decis=='yes':
-    
+
     f_sao=open('marx_sao.rdb','w+')
-    
-    flux=extract_data['NET_RATE']/(arf_data['SPECRESP']*cor_factor)
-    
+
+    flux=extract_data['NET_RATE']/(arf_data['SPECRESP'])
+
     f_sao.write('#argument file for SAOTrace, manually created from marx_extract.spec and marx_corr.arf\n')
     f_sao.write('ENERG_LO\tENERG_HI\tFLUX\n')
     for i in range(len(flux)):
-        
+
         f_sao.write('%f\t%f\t%f\n' %(arf_data['ENERG_LO'][i],arf_data['ENERG_HI'][i],flux[i]))
-    
+
     f_sao.close()
-    
+
     #sao parameters file
-    
+
     fpar_sao=open('marx_sao_par.lua','w+')
     fpar_sao.write('ra_pnt='+ra_nom+'\ndec_pnt='+dec_nom+'\nroll_pnt='+roll_nom+'\n\n'\
                   +'dither_asol_chandra{file="'+ditherfile+'", ra=ra_pnt, dec=dec_pnt, roll=roll_pnt}\n\n'
@@ -246,39 +248,39 @@ if decis=='yes':
                   +'spectrum={{file="marx_sao.rdb", units="photons/s/cm2", scale=1, format="rdb", emin="ENERG_LO",'
                   +'emax="ENERG_HI",flux="FLUX"}}}')
     fpar_sao.close()
-    
+
     #sao not working as of now
-    
+
     os.system('trace-nest tag=saotrace srcpars=marx_sao_par.lua tstart='+date+' limit='+expos+' limit_type=sec')
-    
+
     os.system('punlearn marx')
-    
-    #marx parameters : 
+
+    #marx parameters :
     #SourceFlux=-1 tells marx to take the integrated flux of the Spectrum
     #Verbose=yes to have diagnostic messages
-    
+
     par_marx='pset marx SourceType="SAOSAC" SourceFlux=-1 SAOSACFile=saotrace.fits'\
               +' ExposureTime='+expos+' TStart='+date+' OutputDir=sao_sim GratingType='+grating\
               +' DetectorType="ACIS-S" RA_Nom='+ra_nom+' Dec_Nom='+dec_nom\
               +' DitherModel="FILE" DitherFile='+ditherfile\
-              +' Roll_Nom='+roll_nom+' SourceRA='+source_ra+' SourceDEC='+source_dec+' Verbose=yes mode=h'    
+              +' Roll_Nom='+roll_nom+' SourceRA='+source_ra+' SourceDEC='+source_dec+' Verbose=yes mode=h'
 
 elif decis=='no':
-    
+
     f_marx=open('marx_arg.tbl','w+')
-    
-    fluxdens=extract_data['NET_RATE']/(arf_data['SPECRESP']*cor_factor*float(binning))
-    
+
+    fluxdens=extract_data['NET_RATE']/(arf_data['SPECRESP']*float(binning))
+
     f_marx.write('#argument file for larx, manually created from marx_extract.spec and marx_corr.arf\n')
     for i in range(len(fluxdens)):
-        
+
         f_marx.write('%f %f\n' %(arf_data['ENERG_HI'][i],fluxdens[i]))
-    
+
     f_marx.close()
-    
+
     os.system('punlearn marx')
-    
-    #marx parameters : 
+
+    #marx parameters :
     #SourceFlux=-1 tells marx to take the integrated flux of the Spectrum
     #Verbose=yes to have diagnostic messages
 
@@ -307,21 +309,21 @@ os.system('mkdir -p '+simdirs[decis_int])
 
 for i in range(1,nsim+1):
     os.system('marx')
-    
+
     os.chdir(simdirs[decis_int])
     curr_fitsname=fitsname[decis_int]+str(i)+'.fits'
-    
+
     #running a pileup command to emulate the loss of counts due to pileup
     os.system('marxpileup MarxOutputDir="./"')
     os.chdir('pileup')
-    
+
     #creating a fits file from the outputs. This is the only output we're interested in
     #so we move it and start again (marx and marxpileup both overwrite files by default)
     os.system('marx2fits ./ '+curr_fitsname)
     os.system('mv '+curr_fitsname+' ../../'+outdirs[decis_int])
     os.chdir('../..')
 
-#deleting all the temporary files    
+#deleting all the temporary files
 os.chdir(simdirs[decis_int]+'/pileup')
 os.system('rm *')
 os.chdir('..')
@@ -330,23 +332,23 @@ os.system('rm *')
 os.chdir('..')
 os.system('rmdir '+simdirs[decis_int])
 
-#moving the initialisation files 
+#moving the initialisation files
 os.system('mv *marx* '+outdirs[decis_int])
 
 #%%comparison with the data
 
 '''
-We  divide our regions in nbins (default 10) annuli ranging from the source center to 
+We  divide our regions in nbins (default 10) annuli ranging from the source center to
 twice the maximal dimension of the source region (which should be an ellipse or a circle in sky region)
 
-At the end we renormalize the outer parts of the PSF to reduce errors. 
+At the end we renormalize the outer parts of the PSF to reduce errors.
 The inner limit results from manual computations of the proportion of the initial data affected by pile-up.
 
 We also use our own computation of the source center instead of the source centroid
- 
+
 -> You'll need to change a few things to use ellipsoid annuli
 
-Moreover, the code expects a background region containing a single region (in sky coordinates). 
+Moreover, the code expects a background region containing a single region (in sky coordinates).
 If you use composite background regions you'll need to adapt the very last part
 '''
 
@@ -354,7 +356,7 @@ if type(source_reg).__name__=='EllipseSkyRegion':
     largestsize=np.maximum(source_reg.width,source_reg.height)
 elif type(source_reg).__name__=='CircleSkyRegion':
     largestsize=source_reg.radius
-    
+
 maxsize=largestsize*max_factor
 
 #we will write the command line in pixels to avoid complications
@@ -378,11 +380,11 @@ else :
 f_bg_marx=open('marx_bg.reg','w+')
 f_bg_marx.write(bgreg_line[:bgreg_line.find('#')])
 f_bg_marx.close()
-    
+
 #string used in the command line
 str_annulus='[bin sky=annulus('+str(ctr_x)+','+str(ctr_y)+',0:'\
               +str(maxsize_pixel)+':'+str(step)+')]'
-              
+
 #With this we can extract the entire concentric annuli in one go
 os.system('dmextract "'+evt2+str_annulus+'" bkg="'+evt2+'[bin sky=@marx_bg.reg]"'\
           +' outfile=curr_profile.fits opt=generic clobber=yes')
@@ -392,7 +394,7 @@ with fits.open('curr_profile.fits') as curr_profile :
     sbdata=curr_profile[1].data['SUR_BRI']
     sbdata_err=curr_profile[1].data['SUR_BRI_ERR']
 
-#3 sigma intervals on the initial data (not necessary for the plot but can be called after the script 
+#3 sigma intervals on the initial data (not necessary for the plot but can be called after the script
 #for the values)
 sig1_data=np.transpose(np.array([sbdata-1*sbdata_err,sbdata+1*sbdata_err]))
 sig3_data=np.transpose(np.array([sbdata-3*sbdata_err,sbdata+3*sbdata_err]))
@@ -453,28 +455,28 @@ f_reg.close()
 os.system('punlearn dmextract')
 
 
-#Now, we extract the renormalization annuli and the concentric annuli in a similar manner than with the data, 
+#Now, we extract the renormalization annuli and the concentric annuli in a similar manner than with the data,
 #adding the renormalization
 
 sblist=np.array([[None]*nbins]*nsim)
 sblist_err=np.array([[None]*nbins]*nsim)
 sblist_norm=np.array([None]*nsim)
-             
+
 
 for i in range(1,nsim+1):
     currfile=fitsname[decis_int]+str(i)+'.fits'
-    
+
     os.system('dmextract "'+currfile+str_annulus_renorm+'" outfile=curr_profile.fits opt=generic clobber=yes')
     with fits.open('curr_profile.fits') as curr_profile:
         sblist_norm[i-1]=curr_profile[1].data['SUR_BRI'][0]
-        
+
     os.system('dmextract "'+currfile+str_annulus+'" outfile=curr_profile.fits opt=generic clobber=yes')
     with fits.open('curr_profile.fits') as curr_profile:
         sblist[i-1]=curr_profile[1].data['SUR_BRI']*sbdata_norm/sblist_norm[i-1]
         sblist_err[i-1]=curr_profile[1].data['SUR_BRI_ERR']*sbdata_norm/sblist_norm[i-1]
-    
 
-#ordering and transposing the surface brillance list for easier manipulation    
+
+#ordering and transposing the surface brillance list for easier manipulation
 ord_sblist=np.sort(np.transpose(sblist))
 
 argord_sblist=np.argsort(np.transpose(sblist))
@@ -500,16 +502,16 @@ sig3_list=np.array([[None,None]]*nbins)
 '''Wierdly enough, it seems that the images from Marx are often identical'''
 
 for i in range(nbins):
-    
+
     ord_sblist_err[i]=ord_sblist_err[i][argord_sblist[i]]
-    
+
     sig1_list[i][0]=ord_sblist[i][ind1_lo]-1*ord_sblist_err[i][ind1_lo]
     sig1_list[i][1]=ord_sblist[i][ind1_up]+1*ord_sblist_err[i][ind1_up]
-    
+
     sig3_list[i][0]=ord_sblist[i][ind3_lo]-3*ord_sblist_err[i][ind3_lo]
     sig3_list[i][1]=ord_sblist[i][ind3_up]+3*ord_sblist_err[i][ind3_up]
 
-os.chdir('..')   
+os.chdir('..')
 
 #%% graphs
 
@@ -532,18 +534,18 @@ tr_sig3_data=np.transpose(sig3_data).astype(float)
 limitor=np.where(tr_sig3_data[0]<0,tr_sig3_data[0][0],tr_sig3_data[0])
 
 ax_marx.set_ylim(min(limitor)*0.9,max(limitor)*1.1)
-                      
+
 # for i in range(nbins):
 #     if tr_sig1_list[0][i]<=0:
 #         tr_sig1_list[0][i]=1e-10
 #     if tr_sig1_list[1][i]<0:
 #          tr_sig1_list[1][i]=1e-10
-         
+
 #     if tr_sig3_list[0][i]<=0:
 #         tr_sig3_list[0][i]=1e-10
 #     if tr_sig3_list[1][i]<=0:
 #          tr_sig3_list[1][i]=1e-10
-        
+
 x_axis=np.linspace(max_factor/nbins,max_factor,num=nbins)
 
 ax_marx.fill_between(x_axis, tr_sig3_list[0],tr_sig1_list[0],
