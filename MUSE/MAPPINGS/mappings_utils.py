2353062b822ee78510fe66922886926e1a3a50eb
Parrazyte <82396168+Parrazyte@users.noreply.github.com>
2021-07-19 12:06:46 +0200

    updated to work with all models

--- MUSE/MAPPINGS/mappings_utils.py
+++ MUSE/MAPPINGS/mappings_utils.py
@@ -1,252 +1,106 @@
-# @Author: Maxime Parra
-# @Date:   07-07-2021
+# @Author: mparra
+# @Date:   06-07-2021
 # @Email:  maxime.parra@irap.omp.eu
 # @Last modified by:   mparra
 # @Last modified time: 09-07-2021
 
-import os
-import sys
+
+import os,sys
+sys.path.append('/home/mparra/PARRA/Scripts/Python/MUSE/MAPPINGS/')
+sys.path.append('/home/mparra/PARRA/Scripts/Python/MUSE/')
+sys.path.append('/home/mparra/PARRA/Scripts/Python/MUSE/BPT/')
+import argparse
 import numpy as np
-sys.path.append('/home/mparra/PARRA/Scripts/Python/MUSE/BPT')
-import pandas as pd
-import glob
-import matplotlib as mpl
 
-"""
-Pne2014 and Allen models to be updated (missing some informations in the csvs but the database is down for now so can't check what's missing)
-"""
+from mpdaf.obj import Image
 
-shock_database_path='/home/mparra/PARRA/prog/MAPPINGS/V/shock_database/tables/'
+import bpt_config as bptcfg
+from bpt_utils import bpt_single
+from mappings_utils import shock_model
 
 
-def plot_dgrid(dgrid,axis,BPT,color,label):
-    
-    '''
-    function to plot the 2d grid of a shock_model in a specific BPT diagram
-    
-    alpha decreases (transparency increases) with increasing shock velocities
-    
-    we use two orders of the tables to draw separately (and with different alphas) the segments of the 2d surface 
-    between each shck_vel and each mag_fld parameter
-    
-    A single label is displayed for an alpha of 1
-    '''
-    
-    #sorting the grid in the two directions
-    dgrid_v=dgrid.sort_values(['mag_fld','shck_vel'])
-    dgrid_h=dgrid.sort_values(['shck_vel','mag_fld'])
-    
-    #getting the range of parameters
-    vel_range=np.unique(dgrid.shck_vel)
-    b_range=np.unique(dgrid.mag_fld)
-    
-    #and our alpha array
-    alpha_arr=np.logspace(0,-1,len(vel_range))
-    
-    #first direction of the mesh, here we can do a single loop since our line are grouped by shock velocity and thus the 
-    #grid_h segments have a single alpha
-    for i in range(len(vel_range)):
-        grid_h=dgrid_h[dgrid_h['shck_vel']==vel_range[i]]
-        grid_h.plot(ax=axis,x=BPT.x_column,y=BPT.y_column,color=color,alpha=alpha_arr[i],legend=False,label=label if i==0 else '')
-    
-    #second direction of the mesh, here we need to do two loops since every segment in a line has a different alpha
-    for i in range(len(b_range)):
-        grid_v=dgrid_v[dgrid_v['mag_fld']==b_range[i]]
-        for j in range(len(vel_range)):
-            segment_v=grid_v.iloc[j:j+2]
-            segment_v.plot(ax=axis,x=BPT.x_column,y=BPT.y_column,color=color,alpha=alpha_arr[j],legend=False,label='')
-            
-def plot_shock_mod(models,axis,BPT,labels):
-    
-    '''
-    function to plot a list of models in a specific BPT diagram
-    
-    "models" should be either a dataframe or a list of dataframes
-    In order to plot 2d surfaces correctly, keep one density/abundance/carbon to dust ratio for all the dataframes
-    Also preferably have only one parameter varying in the array (since there is only one color scale)
-    (e.g. densities or abundances)
-    
-    we use the plot_dgrid function to plot with variable transparency (here increasing with the shock speed values)
-    
-    viridis color map by default
-    
-    labels is assumed to be the same length as the models 
-    '''
-    viridis=mpl.cm.get_cmap('viridis',len(models))
-    col_vars=viridis(range(len(models)))
-    
-    for i in range(len(models)):    
-        curr_map_model=models[i]
-        plot_dgrid(curr_map_model,axis,BPT,col_vars[i],labels[i])
-            
-def shock_model(modelname):
-      
-    """
-    General function for the MAPPINGSV models, which uses the tables from the 3mdb database downloaded with gridmaker.
-    Contains subclasses of different models depending on the abundance
-    
-    Parameter
-    ----------
-    model: str
-        Gives which type of model is used here
-        
-    Arguments
-    ---------
-    csv: csv array
-        Returns the list of csvs for every file associated to this model in the tables directory
-    
-    abund: str/float array
-        Returns the list of abundances available for each model. 
-            -If the abundances are floats, you can access the corresponding models with the .abund(value) attribute, 
-            which returns a class containing the model
-            -If the abundances are str, you can directly access the the models with the .yourabundancestrings attributes
-        
-    """
-    
-    #tables directory
-    currdir=os.getcwd()
-    os.chdir(shock_database_path)
-    
-    #getting all the model files corresponding to this model name
-    modelfiles=glob.glob(modelname+"*.csv")
-    
-    #and storing the csvs and model parameters in an array
-    csv_list=np.array([None]*len(modelfiles))
-    abund_list=np.array([None]*len(modelfiles))
-    abund_strlist=np.array([None]*len(modelfiles))
-    
-    for i in range(len(modelfiles)):
-        
-        filename=modelfiles[i]
-        csv_list[i]=pd.read_csv(filename,sep='\t')
-        
-        #searching for the abundances
-        if 'Allen' in modelname:
-            abund_list[i]=filename[filename.find('_')+1:filename.find('.csv')]
-        
-        elif 'Gutkin' in modelname:
-            abund_strlist[i]=filename[filename.find('d')+1:filename.find('C')-1]
-            
-        elif '3MdB' or 'PNe' in modelname:
-            #for the PNe model the only abundance is solar
-            abund_list[i]='solar'
-     
-    #for the Gutkin model we can get rid of the abundances repetitions using floats for the abundances
-    if 'Gutkin' in modelname:
-        #float abundances
-        abund_float=np.core.defchararray.add('0.',abund_strlist)
-        abund_float=np.sort(np.unique(abund_float.astype(float)))
-        #string abundances used for other purposes
-        abund_list=abund_float.astype(str).tolist()
-        abund_list=np.array([elem.replace('.','_') for elem in abund_list])
-                
-    class shock_model_single:
-        """
-       subclass for a single model for a specific abundance (and dust ratio if it is variable)
-       Parameters
-       ----------
-       csv: 
-           Gives the csv to use
-       """
-        
-        def __init__(self, csv):
-            if len(csv)==1:
-                self.csv=csv[0]
-            else:
-                self.csv=csv
-            self.dens=np.unique(self.csv['dens'])
-            self.mag_fld=np.unique(self.csv['mag_fld'])
-            self.shck_vel=np.unique(self.csv['shck_vel'])
-            
-        #one dimensionnal grid (lines) functions
-        def grid(self, dens,mag_fld):
-            return self.csv[self.csv.dens==dens][self.csv.mag_fld==mag_fld][['shck_vel','OIII_Hb','NII_Ha','SII_Ha','OI_Ha']]
-    
-        def lgrid(self, dens,mag_fld):
-            #we keep the velocity in linear and check for the presnece of infs. to transform into nans.
-            log_csv=self.csv[self.csv.dens==dens][self.csv.mag_fld==mag_fld][['shck_vel','OIII_Hb','NII_Ha','SII_Ha','OI_Ha']]
-            log_csv=log_csv.copy()
-            log_csv=log_csv.replace([np.inf,-np.inf],np.nan)
-            log_csv[log_csv.columns[1:]]=np.log10(log_csv[log_csv.columns[1:]])
-            return log_csv
-            
-        #two dimensionnal surfaces grids functions
-        def d_grid(self,dens):
-            return self.csv[self.csv.dens==dens][['mag_fld','shck_vel','OIII_Hb','NII_Ha','SII_Ha','OI_Ha']],
+'''
+This script compares the current results of the MAPPINGSV shock table database to BPT diagrams
 
-        def d_lgrid(self,dens):
-            #we keep the velocity in linear and check for the presnece of infs. to transform into nans.
-            log_csv=self.csv[self.csv.dens==dens][['mag_fld','shck_vel','OIII_Hb','NII_Ha','SII_Ha','OI_Ha']]
-            log_csv=log_csv.copy()
-            log_csv=log_csv.replace([np.inf,-np.inf],np.nan)
-            log_csv[log_csv.columns[2:]]=np.log10(log_csv[log_csv.columns[2:]])
-            return log_csv
-        
-    class shock_mod:
-        
-        """
-        Global model class associated with the function shock_model
-        contains information on the entire model and enables access to the more specific elements
-        """
-        
-        def __init__(self):
-            
-            #list of the csvs
-            self.csv=csv_list
-            
-            #list of STRING abundances
-            self.abund=abund_list 
-            if 'Gutkin' in modelname:
-                #list of float abundances specifically for the Gutkin models
-                self.abund_float=abund_float
-                for i in range(len(abund_list)):
-                    abund_var=abund_float[i]
-                    
-                    #dynamically adding attributes for the float abundances values, they can't be called except with getattr() 
-                    #We'll rather directly abund_single here but we still create it just in case
-                    setattr(self,str(abund_list[i]),self.abund_single(abund_var))
-            else:
-                for i in range(len(abund_list)):
-                    #dynamically adding attributes for the str abundance values
-                    setattr(self,abund_list[i],self.abund_single(abund_list[i]))
-                    
-        def abund_single(self,abundance):
-            
-            """
-            Subwrapper for carbon to dust proportion specification for Gutkin
-            Parameters
-            ----------
-            
-            abund: str
-                Set of abundances corresponding to the model
-            """
-            
-            class shock_model_with_abundances:
-                def __init__(self):
-                    #dichotomy since Gutkin models will have 
-                    if type(abundance)==float or np.float64:
-                        #here we recheck in the file list to get the csvs corresponding to the different carbon to dust fraction
-                        abund_allfloat=np.core.defchararray.add('0.',abund_strlist)
-                        abund_allfloat=abund_allfloat.astype(float)
-                        positions=np.where(abund_allfloat==abundance)[0]
-                        
-                        #now we can determine who is where and attribute the corresponding attributes
-                        for i in positions:
-                            if '0d26' in modelfiles[i]:
-                                self.ctd_low=shock_model_single(csv_list[i])
-                            if '1d00' in modelfiles[i]:
-                                self.ctd_high=shock_model_single(csv_list[i])
-                                
-                    elif type(abundance)==str or type(np.array(abundance)[0]):
-                        position=np.where(abund_list==abundance)
-                        
-                        #here since we have one less "level" of classes due to the absence of carbon to dust ratio variability
-                        #we directly replace the class parameters by the single shock model class
-                        self.__dict__=shock_model_single(csv_list[position]).__dict__
-                        self.__class__=shock_model_single(csv_list[position]).__class__
-                        
-            return shock_model_with_abundances()
+for now we keep a single model (default: Gutkin) and a single density (default: 1/cm^3) per iteration to ensure visibility. 
+
+The Gutkin model is the only one available for now (see mappings_utils).
+
+The carbon to dust ratio for the Gutkin model is fixed to high.
+
+The range of shock speed displayed can be modified. The default is 100-250 km/s
+Note that the Gutkin model ranges from 100 to 1000 km/s in steps of 25.
+'''
+
+# plt.style.use('/home/agurpide/.config/matplotlib/stylelib/paper.mplstyle')
+
+ap = argparse.ArgumentParser(description='Script to read and plot the output of the mappings V libraries (Allen et al. 2008)')
+ap.add_argument("-m", "--models", nargs='*', help="Model to be compared to the data", default='Gutkin')
+ap.add_argument("-o", "--outdir", nargs='?', help="Output dir", default='mappings', type=str)
+ap.add_argument("-r", "--regions", nargs='*', help="Region files to be overlaid on the image", default=None)
+ap.add_argument("-c", "--contours", nargs='?', help="Fits file to use to overlay contours", type=str)
+ap.add_argument("-d", "--density", nargs='?', help="density to use in per cubic cm", type=float,default=1)
+ap.add_argument("-vmin", "--minspeed", nargs='?', help="minimal shock speed velocity to be displayed", type=float,default=100)
+ap.add_argument("-vmax", "--maxspeed", nargs='?', help="max shock speed velocity to be displayed", type=float,default=250)
+args = ap.parse_args()
+
+#renaming arguments
+outdir=args.outdir
+if not os.path.isdir(outdir):
+    os.mkdir(outdir)
+
+model= args.models
+dens=args.density
+
+bpt_type = bptcfg.type
+lineratiomaps = [bptcfg.lineratio_paths["n2_ha"], bptcfg.lineratio_paths["s2_ha"], bptcfg.lineratio_paths["oI_ha"]]
+
+#range of speeds used
+vmin=args.minspeed
+vmax=args.maxspeed
+speeds = np.linspace(vmin,vmax,num=int((vmax-vmin)/25)+1)
+
+#creating the models
+shock_tables=shock_model(model)
+
+#getting the abundances and their solar fraction
+abunds=shock_tables.abund_float
+abunds_frac=abunds/0.01524
+
+# use_pos=np.where((abunds_frac<=1) & (abunds_frac>0.01))
+#instead of the line above we restrict manually to a few values for less overlap
+use_pos=np.array([4,7,10])
+abunds_use=abunds[use_pos]
+
+#creating the labels which we will use for the grids
+labels_use0=np.array([r"$Z_{ISM}$ = "]*len(abunds_use))
+labels_use1=np.char.add(abunds_use.astype(str),np.array([' i.e. ']*len(abunds_use)))
+labels_use2=np.char.add(abunds_frac.round(decimals=3)[use_pos].astype(str),np.array([r" $Z_\odot$"]*len(abunds_use)))
+labels_use=np.char.add(labels_use1,labels_use2)
+labels_use=np.char.add(labels_use0,labels_use)
+
+#putting the models of interest in an array
+mod_use=np.array([None]*len(abunds_use))
+for i in range(len(abunds_use)):
+    #getting the the 2d grids for the abundance, with the correct density
+    mod_use[i]=shock_tables.abund_single(abunds_use[i]).ctd_high.d_lgrid(dens)
     
-    #returning to the initial directory
-    os.chdir(currdir)
-    return shock_mod()
+    #restricting the shock velocity to the ones inputted
+    mod_use[i]=mod_use[i][mod_use[i]['shck_vel'].isin(speeds)]
+    
+title="BPT compared to a range of "+model+" shock models, with n="+str(dens)+" cm$^{-3}$ and shock speeds in ["+str(vmin)+","+str(vmax)+"] km/s"
+
+#computations
+ymap = Image(bptcfg.lineratio_paths["o3_hb"])
+logy = np.log10(ymap.data.data)
+colormap = ["Blues", "Greens", "Purples", "Oranges"]
+
+for i, ratiomap in enumerate(lineratiomaps):
+    
+    bpt_type = i + 1
+    bpt_fits, bpt_indexes, figure = bpt_single(ratiomap, logy, args.regions, args.contours, bpt_type, colormap,title=title,
+                                                shock_mods=mod_use,mod_labels=labels_use,figsize='big',bpt_labels=False)
+
+    outfile = "%s/mapV_bpt%d_d%d.png" % (outdir, bpt_type,dens)
+    figure.savefig(outfile, format="png", bbox_inches="tight", pad_inches=0.4)
+    print("Results stored to %s" % outfile)
