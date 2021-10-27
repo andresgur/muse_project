eefbbf9ebd2e83c536664aca49f06a98e409a1ea
andresgur <38692538+andresgur@users.noreply.github.com>
2021-10-27 13:46:58 +0200

    Optimized code by merging the two if sections
    Tested for S3D at least

--- MUSE/utils/H2diags.py
+++ MUSE/utils/H2diags.py
@@ -11,7 +11,6 @@ papers :
 Pilyugin et al. 2016 : https://academic.oup.com/mnras/article/457/4/3678/2589035
 Dors et al. 2017 : doi.org/10.1093/mnras/stw3115
 Law et al. 2021 : https://arxiv.org/abs/2011.06012
-
 Note : this code will automatically search for automatic bpt outputs from bpt_newer.py
 '''
 
@@ -27,6 +26,34 @@ import numpy as np
 import logging
 import argparse
 from line_utils import ratio_maker, map_plot
+
+#defining the calibration functions. The arguments are ordered according to the lines wavelengths
+
+def OH_R(R2,N2,R3):
+
+    OH_RU=8.589+0.022*np.log(R3/R2)+0.399*np.log(N2)+(-0.137+0.164*np.log(R3/R2)+0.589*np.log(N2))*np.log(R2)
+    OH_RL=7.932+0.944*np.log(R3/R2)+0.695*np.log(N2)+(+0.970-0.291*np.log(R3/R2)-0.019*np.log(N2))*np.log(R2)
+
+    return np.where(np.log(N2)>=-0.6,OH_RU,OH_RL)
+
+def OH_S(N2,S2,R3):
+
+    OH_SU=8.424+0.030*np.log(R3/S2)+0.751*np.log(N2)+(-0.349+0.182*np.log(R3/S2)+0.508*np.log(N2))*np.log(S2)
+    OH_SL=8.072+0.789*np.log(R3/S2)+0.726*np.log(N2)+(+1.069-0.170*np.log(R3/S2)+0.022*np.log(N2))*np.log(S2)
+
+    return np.where(np.log(N2)>=-0.6,OH_SU,OH_SL)
+
+def OH_R_2D(R2,N2):
+
+    OH_RU_2D=8.589+0.329*np.log(N2)+(-0.205+0.549*np.log(N2))*np.log(R2)
+
+    return np.where(np.log(N2)>=-0.6,OH_RU_2D,np.nan)
+
+def OH_S_2D(N2,S2):
+
+    OH_SU_2D=8.445+0.699*np.log(N2)+(-0.253+0.217*np.log(N2))*np.log(S2)
+
+    return np.where(np.log(N2)>=-0.6,OH_SU_2D,np.nan)
 
 ap = argparse.ArgumentParser(description='Create BPT diagram from two given line ratio maps and the BPT diagram type. Separatios based on Law et al. 2021')
 ap.add_argument("-o", "--outdir", nargs='?', help="Output dir", default='H2_diags', type=str)
@@ -97,17 +124,19 @@ if pilyugin_results[1]=='Unavailable':
     sys.exit()
 
 else:
-
+    metal_map=metal_file[0].data
     #We got everything
     if pilyugin_results[0]==pilyugin_results[3]=='Done':
             print('Computing using the standard formulae')
             case='R,3D'
+            metal_map=OH_R(ratio_R2,ratio_N2,ratio_R3)
 
     #R2 line is missing
     if pilyugin_results[0]=='Unavailable' and pilyugin_results[2]==pilyugin_results[3]=='Done':
             print('R2 ratio unavailable.\n'
               'Computing using the S2 ratio instead.\n')
             case='S,3D'
+            metal_map=OH_S(ratio_N2,ratio_S2,ratio_R3)
 
     #two lines are missing (2 cases)
     if pilyugin_results[2]==pilyugin_results[3]=='Unavailable':
@@ -115,59 +144,21 @@ else:
               'Attempting a two-dimensional computation for the higher values of the NII ratios.\n'
               'Using the R relation\n')
         case='R,2D'
+        metal_map=OH_R_2D(ratio_R2,ratio_S2)
 
     if pilyugin_results[0]==pilyugin_results[3]=='Unavailable':
         print('Some of the ratios are unavailable.\n'
               'Attempting a two-dimensional computation for the higher values of the NII ratios.\n'
               'Using the S relation\n')
         case='S,2D'
+        metal_map=OH_S_2D(ratio_N2,ratio_S2)
 
+print("Metallicity computed using the %s method" %case)
 
-#defining the calibration functions. The arguments are ordered according to the lines wavelengths
-
-def OH_R(R2,N2,R3):
-
-    OH_RU=8.589+0.022*np.log(R3/R2)+0.399*np.log(N2)+(-0.137+0.164*np.log(R3/R2)+0.589*np.log(N2))*np.log(R2)
-    OH_RL=7.932+0.944*np.log(R3/R2)+0.695*np.log(N2)+(+0.970-0.291*np.log(R3/R2)-0.019*np.log(N2))*np.log(R2)
-
-    return np.where(np.log(N2)>=-0.6,OH_RU,OH_RL)
-
-def OH_S(N2,S2,R3):
-
-    OH_SU=8.424+0.030*np.log(R3/S2)+0.751*np.log(N2)+(-0.349+0.182*np.log(R3/S2)+0.508*np.log(N2))*np.log(S2)
-    OH_SL=8.072+0.789*np.log(R3/S2)+0.726*np.log(N2)+(+1.069-0.170*np.log(R3/S2)+0.022*np.log(N2))*np.log(S2)
-
-    return np.where(np.log(N2)>=-0.6,OH_SU,OH_SL)
-
-def OH_R_2D(R2,N2):
-
-    OH_RU_2D=8.589+0.329*np.log(N2)+(-0.205+0.549*np.log(N2))*np.log(R2)
-
-    return np.where(np.log(N2)>=-0.6,OH_RU_2D,np.nan)
-
-def OH_S_2D(N2,S2):
-
-    OH_SU_2D=8.445+0.699*np.log(N2)+(-0.253+0.217*np.log(N2))*np.log(S2)
-
-    return np.where(np.log(N2)>=-0.6,OH_SU_2D,np.nan)
 
 #computing the results depending of the case
 
 os.chdir(outdir)
-
-metal_map=metal_file[0].data
-
-if case=='R,3D':
-    metal_map=OH_R(ratio_R2,ratio_N2,ratio_R3)
-    
-if case=='S,3D':
-    metal_map=OH_S(ratio_N2,ratio_S2,ratio_R3)
-    
-if case=='R,2D':
-    metal_map=OH_R_2D(ratio_R2,ratio_S2)
-
-if case=='S,2D':
-    metal_map=OH_S_2D(ratio_N2,ratio_S2)
 
 if bpt_map[0].data.shape!=metal_map.shape:
     logging.warning("Can't compare the bpt map and the metal map : Their shapes are different.\n"
@@ -179,7 +170,7 @@ metal_file[0].data=metal_map
 
 metal_file.writeto('metal_map.fits',overwrite=True)
 
-metal_title='metal map computed with method '+case
+metal_title='metal map computed with method '+ case
 
 map_plot('metal_map.fits',title=metal_title,contours=args.contours,regions=args.regions)
 
@@ -212,3 +203,5 @@ ion_par_file.writeto('ion_par.fits',overwrite=True)
 ion_title='ionization map computed with method' +case
 
 map_plot('ion_par.fits',title=ion_title,contours=args.contours,regions=args.regions)
+
+print("Results stored to %s" %outdir)
