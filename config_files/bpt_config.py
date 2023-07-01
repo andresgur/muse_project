4dcf6c79552d9cea47ab2df6f1d3f55438b595ec
andresgur <38692538+andresgur@users.noreply.github.com>
2023-07-01 12:45:19 +0100

    Updated config parser for bpt_colored.py

--- config_files/bpt_config.py
+++ config_files/bpt_config.py
@@ -1,19 +1,22 @@
-# @Author: Andrés Gúrpide <agurpide>
+# @Author: Andrés Gúrpide <andresgur>
 # @Date:   30-04-2021
-# @Email:  agurpidelash@irap.omp.eu
-# @Last modified by:   agurpide
+# @Email:  andresgurlash@irap.omp.eu
+# @Last modified by:   andresgur
 # @Last modified time: 30-04-2021
 # Config file for the BPT diagram code
 
+[Paths]
+ulx_dir:/home/andresgur/optical_data/NGC247/
+base_dir:${ulx_dir}/lineratios
+o3_hb: ${base_dir}/OIII5007_HBETAratio.fits
+n2_ha: ${base_dir}/NII6583_HALPHAratio.fits
+oI_ha: ${base_dir}/OI6300_HALPHAratio.fits
+s2_ha: ${base_dir}/SII_HALPHAratio.fits
 
-lineratio_paths = {
-    "o3_hb": "/home/agurpide/optical_data/NGC1313/nancleancubes/coordadjusted/lineratios/OIII5007_HBETAratio.fits",
-    "n2_ha": "/home/agurpide/optical_data/NGC1313/nancleancubes/coordadjusted/lineratios/NII6583_HALPHAratio.fits",
-    "oI_ha": "/home/agurpide/optical_data/NGC1313/nancleancubes/coordadjusted/lineratios/OI6300_HALPHAratio.fits",
-    "s2_ha": "/home/agurpide/optical_data/NGC1313/nancleancubes/coordadjusted/lineratios/SII_HALPHAratio.fits"
-}
-# type can be law (Law et al. 2021) or kewley (Kewley et al. 2006)
-type = "law"
 
+[bpt]
+# diagram can be law (Law et al. 2021) or kewley (Kewley et al. 2006)
+diagram = law
+[z_values]
 # path for a 2D fits file to give intensity to the individual points in the BPT, it can be set to None and it will be ignored
-z_values = "/home/agurpide/optical_data/NGC1313/nancleancubes/coordadjusted/camel_1_n2ha/cleaned_images/cleancamel_1_n2ha_ssmooth_disp_indep_HALPHA.fits"
+z_values = ${Paths:ulx_dir}/camel_1_n2ha/cleaned_images/cleancamel_1_n2ha_ssmooth_wavedisp_indep_HALPHAcorrected_wcsupdated.fits
