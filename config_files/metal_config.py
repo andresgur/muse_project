6dd22ca2c63aa305269b6cb14bf6d81750f3303c
andresgur <38692538+andresgur@users.noreply.github.com>
2023-07-18 17:19:42 +0100

    Added config file for H2diags.py

---
+++ config_files/metal_config.py
@@ -0,0 +1,13 @@
+# @Author: Andrés Gúrpide <agurpide>
+# @Date:   30-04-2021
+# @Email:  agurpidelash@irap.omp.eu
+# @Last modified by:   agurpide
+# @Last modified time: 30-04-2021
+# Config file for the BPT diagram code
+
+[lineratio_paths]
+ulx_dir:/home/andresgur/optical_data/NGC247/stacked_cube/coordadjusted
+base_dir:${ulx_dir}/lineratios
+N_2: ${base_dir}/NII_HBETA_ratio.fits
+S_2: ${base_dir}/SII_HBETA_ratio.fits
+R_3: ${base_dir}/OIII_HBETA_ratio.fits
