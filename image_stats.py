33ae736a861cec41c01c62011ac4b116973ce96b
Andrés Gúrpide Lasheras <38692538+andresgur@users.noreply.github.com>
2023-10-24 11:56:20 +0100

    Updated deprecated read_regions

---
+++ image_stats.py
@@ -0,0 +1,68 @@
+# @Author: Andrés Gúrpide <agurpide>
+# @Date:   22-06-2021
+# @Email:  agurpidelash@irap.omp.eu
+# @Last modified by:   agurpide
+# @Last modified time: 10-02-2022
+
+import argparse
+from regions import Regions
+import numpy as np
+from mpdaf.obj import Image
+import muse_utils as mu
+import pyregion
+
+ap = argparse.ArgumentParser(description='Compute stats of input images over the whole FOV or certain regions.')
+ap.add_argument("images", nargs='+', help='List of input images', type=str)
+ap.add_argument("-r", "--region", nargs='?', help='ds9 region with one or more regions in it. Only annulus, circles and ellipses regions are valid',
+                type=str)
+args = ap.parse_args()
+header = "#region\tmean\tstd\tmedian\tsum\tsum_2\tmax\tmin\tarea\n"
+for image in args.images:
+    try:
+        img = Image(image)
+    except ValueError:
+        img = Image(image, ext=1)
+    outstring = header
+    if args.region is not None:
+        region_list = Regions.read(args.region, format="ds9")
+        pyregions = pyregion.open(args.region)
+        for region, (i, pyreg) in zip(region_list, enumerate(pyregions)):
+            # mask everything outside the region
+            sp = pyregion.core.ShapeList([pyreg])
+            mask = sp.get_mask(img.get_data_hdu())
+            img.mask = ~mask
+            # mask nan values
+            mask_sel = np.where(np.isnan(img.data))
+            img.mask_selection(mask_sel)
+            mean = np.ma.mean(img.data)
+            median = np.ma.median(img.data)
+            std = np.ma.std(img.data)
+            sum = np.ma.sum(img.data)
+            sum_2 = np.ma.sum(img.data ** 2)
+            max = np.ma.max(img.data)
+            min = np.ma.min(img.data)
+            try:
+                area = mu.region_to_aperture(region, img.wcs.wcs).area
+            except AttributeError:
+                area = 0
+            img.unmask()
+            # use the name given to the region, otherwise number it
+            if "label" in region.meta:
+                region_name = region.meta["label"]
+            else:
+                region_name = "reg%d" % i
+            outstring += "%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n" % (region_name, mean, std, median, sum, sum_2, max, min, area)
+
+    else:
+        mean = np.ma.mean(img.data)
+        median = np.ma.median(img.data)
+        std = np.ma.std(img.data)
+        sum = np.ma.sum(img.data)
+        sum_2 = np.ma.sum(img.data ** 2)
+        max = np.ma.max(img.data)
+        min = np.ma.min(img.data)
+        outstring += "%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n" % ("image", mean, std, median, sum, sum_2, max, min)
+    outfile = image.replace(".fits", ".dat")
+    with open(outfile, "w+") as f:
+        f.write(outstring)
+    print("Output stored to %s" % outfile)
