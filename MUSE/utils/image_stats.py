05519f521577397ce3d13e2f21708c06c14809d0
Andrés Gúrpide Lasheras <38692538+andresgur@users.noreply.github.com>
2025-08-18 13:22:07 +0200

    Added more stats and histogram, simplified code

--- MUSE/utils/image_stats.py
+++ MUSE/utils/image_stats.py
@@ -5,27 +5,46 @@
 # @Last modified time: 10-02-2022
 
 import argparse
-from regions import read_ds9
+from regions import Regions
 import numpy as np
 from mpdaf.obj import Image
 import muse_utils as mu
 import pyregion
+from astropy.stats import sigma_clipped_stats
+import matplotlib.pyplot as plt
+
+
+def get_stats(maskeddata):
+    """Get the statistics of the masked data"""
+    mean = np.ma.mean(maskeddata)
+    median = np.ma.median(maskeddata)
+    std = np.ma.std(maskeddata)
+    validpixels = np.ma.count(maskeddata)
+
+    std_err = std / (validpixels)**0.5
+    sum = np.ma.sum(maskeddata)
+    sum_2 = np.ma.sum(maskeddata ** 2)
+    max = np.ma.max(maskeddata)
+    min = np.ma.min(maskeddata)
+    clipped_mean, clipped_median, clipped_std  = sigma_clipped_stats(maskeddata, sigma=10, maxiters=20)
+    clipped_std_err = clipped_std / (validpixels)**0.5
+    return mean, std, std_err, median, sum, sum_2, max, min, validpixels, clipped_mean, clipped_median, clipped_std, clipped_std_err
 
 ap = argparse.ArgumentParser(description='Compute stats of input images over the whole FOV or certain regions.')
 ap.add_argument("images", nargs='+', help='List of input images', type=str)
-ap.add_argument("-r", "--regions", nargs='?', help='Region ds9 with one or more regions in it. Only annulus, circles and ellipses regions are valid',
+ap.add_argument("-r", "--region", nargs='?', help='ds9 region file with one or more regions in it. Only annulus, circles and ellipses regions are valid',
                 type=str)
 args = ap.parse_args()
-header = "#region\tmean\tstd\tmedian\tsum\tsum_2\tmax\tmin\tarea\n"
+header = "#region\tmean\tstd\tstd_err\tmedian\tsum\tsum_2\tmax\tmin\tclip_mean\tclip_median\tclip_std\tclip_std_err\tarea\tvalidpixels\n"
 for image in args.images:
     try:
         img = Image(image)
     except ValueError:
         img = Image(image, ext=1)
     outstring = header
-    if args.regions is not None:
-        region_list = read_ds9(args.regions)
-        pyregions = pyregion.open(args.regions)
+    if args.region is not None:
+        region_list = Regions.read(args.region, format="ds9")
+        pyregions = pyregion.open(args.region)
         for region, (i, pyreg) in zip(region_list, enumerate(pyregions)):
             # mask everything outside the region
             sp = pyregion.core.ShapeList([pyreg])
@@ -34,13 +53,7 @@ for image in args.images:
             # mask nan values
             mask_sel = np.where(np.isnan(img.data))
             img.mask_selection(mask_sel)
-            mean = np.ma.mean(img.data)
-            median = np.ma.median(img.data)
-            std = np.ma.std(img.data)
-            sum = np.ma.sum(img.data)
-            sum_2 = np.ma.sum(img.data ** 2)
-            max = np.ma.max(img.data)
-            min = np.ma.min(img.data)
+            mean, std, std_err, median, sum, sum_2, max, min, validpixels, clipped_mean, clipped_median, clipped_std, clipped_std_err = get_stats(img.data)
             try:
                 area = mu.region_to_aperture(region, img.wcs.wcs).area
             except AttributeError:
@@ -51,18 +64,23 @@ for image in args.images:
                 region_name = region.meta["label"]
             else:
                 region_name = "reg%d" % i
-            outstring += "%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n" % (region_name, mean, std, median, sum, sum_2, max, min, area)
-
+            outstring += "%s\t%.3f\t%.2f\t%.4f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.4f\t%.4f\t%.4f\t%.4f\t%d\t%d\n" % (region_name, mean, std, std_err, median, sum, sum_2, max, min, clipped_mean, clipped_median, clipped_std, clipped_std_err, area, validpixels)
+            plt.hist(img.data[~img.mask], label=region_name, alpha=0.5, edgecolor='black')
+            plt.axvline(np.percentile(img.data[~img.mask], 95), color='red', linestyle='--', label='95th percentile')
+            plt.axvline(clipped_mean, color='green', linestyle='--', label='Clipped mean')
+            
     else:
-        mean = np.ma.mean(img.data)
-        median = np.ma.median(img.data)
-        std = np.ma.std(img.data)
-        sum = np.ma.sum(img.data)
-        sum_2 = np.ma.sum(img.data ** 2)
-        max = np.ma.max(img.data)
-        min = np.ma.min(img.data)
-        outstring += "%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\n" % ("image", mean, std, median, sum, sum_2, max, min)
-    outfile = image.replace(".fits", ".dat")
+        mean, std, std_err, median, sum, sum_2, max, min, validpixels, clipped_mean, clipped_median, clipped_std, clipped_std_err = get_stats(img.data)
+
+        outstring += "%s\t%.3f\t%.2f\t%.4f\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.4f\t%.4f\t%.4f\t%.2f\t%d\t%d\n" % ("image", mean, std, std_err, median, sum, sum_2, max, min, clipped_mean, clipped_median, clipped_std, clipped_std_err, 0, validpixels)
+        plt.hist(img.data[~img.mask], alpha=0.5, edgecolor='black')
+        plt.axvline(np.percentile(img.data[~img.mask], 95), color='red', linestyle='--', label='95th percentile')
+        plt.axvline(clipped_mean, color='green', linestyle='--', label='Clipped mean')
+    outfile = image.replace(".fits", "_stats.dat")
     with open(outfile, "w+") as f:
         f.write(outstring)
     print("Output stored to %s" % outfile)
+    plt.xlabel("Instances")
+    plt.legend()
+    plt.yscale("log")
+    plt.savefig(image.replace(".fits", "_stats.png"))
