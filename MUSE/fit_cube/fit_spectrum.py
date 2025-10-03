213ea1e714db94db896e48bfd407015214946778
Andrés Gúrpide Lasheras <38692538+andresgur@users.noreply.github.com>
2025-10-03 14:55:15 +0200

    Added logging of the polynomial degree for the continuum

--- MUSE/fit_cube/fit_spectrum.py
+++ MUSE/fit_cube/fit_spectrum.py
@@ -2,15 +2,13 @@
 # -*- coding: utf-8 -*
 import numpy as np
 import argparse, os, logging
-from line import Lines
-from lineutils import get_instrument_FWHM, compute_shift, ckms, correct_FWHM
-from fitutils import fit_spectrum, DofError, SpectrumMaskedError, plot_fit, get_error
+from fitting.line import Lines
+from fitting.lineutils import get_instrument_FWHM, peculiar_velocity
+from fitting.fitutils import fit_spectrum, DofError, SpectrumMaskedError, plot_fit
 from mpdaf.obj import Spectrum
 import numpy.ma as ma
 from lmfit.model import save_model
 from lmfit.printfuncs import ci_report
-from math import pi
-import matplotlib.pyplot as plt
 
 
 logger = logging.getLogger('fit_spectrum')
@@ -28,9 +26,15 @@ def setup_logging(logfile, loglevel=logging.DEBUG):
     )
 
 logging.getLogger('matplotlib').setLevel(logging.WARNING)
-CATALOG_LINES = Lines().lines
+CATALOG_LINES = Lines()
+ckms = 299792.458
+sigma_to_fwhm = 2.355  # FWHM = 2.355 * sigma
 sqrt2pi = (2. * np.pi) ** 0.5
-CM_TO_PC = 3.24077929e-19
+
+# Set random seeds for reproducibility
+np.random.seed(42)
+import random
+random.seed(42)
 
 def check_input_lines(linegroups):
     """Check if the input lines are valid and exist in the catalog, and are in ascending wavelength order
@@ -41,15 +45,15 @@ def check_input_lines(linegroups):
         
         # Check if lines exist in catalog
         for line in lines_in_group:
-            if line not in CATALOG_LINES:
-                raise ValueError(f"Line {line} not found in catalog. Please check the line name or add it to the catalog. Available lines: {CATALOG_LINES.keys()}")
+            if line not in CATALOG_LINES.lines:
+                raise ValueError(f"Line {line} not found in catalog. Please check the line name or add it to the catalog. Available lines: {CATALOG_LINES.lines.keys()}")
         
         # Check if lines are in ascending wavelength order
         if len(lines_in_group) > 1:
-            wavelengths = [CATALOG_LINES[line].wave for line in lines_in_group]
+            wavelengths = [CATALOG_LINES.lines[line].wave for line in lines_in_group]
             if wavelengths != sorted(wavelengths):
                 raise ValueError(f"Lines in group '{group}' are not in ascending wavelength order. "
-                               f"Current order: {', '.join([f'{line}({CATALOG_LINES[line].wave:.1f}Å)' for line in lines_in_group])}. "
+                               f"Current order: {', '.join([f'{line}({CATALOG_LINES.lines[line].wave:.1f}Å)' for line in lines_in_group])}. "
                                f"Please reorder them by wavelength.")
     
     return True
@@ -61,19 +65,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Fit data cube using voronoi binning')
     parser.add_argument("-d", "--degree", nargs='?', help="Polynomial degree for the continuum", default=1, type=int)
     parser.add_argument("-z", "--redshift", nargs='?', help="Initial guess for the redshift", default=0.0, type=float)
-    parser.add_argument("-D", "--distance", nargs='?', help="Optional distance in Mpc to estimate line luminosities", type=float)
     parser.add_argument("-s", "--sigma", nargs='?', help="Initial guess for the width (sigma) of the lines in Angstroms. Default 1.4 Angstroms", default=1.4, type=float)
     parser.add_argument("-o", "--outdir", nargs='?', help="Name of the output directory", default="fit_spectrum", type=str)
     parser.add_argument("input_spectrum", help="Path to input spectrum to be analysed", type=str)
     parser.add_argument("-w", "--window", help="+- Angstroms to cut the spectrum around each line centroid. Default 25 Angstroms. Reduce for lines close to telluric lines (e.g. [OI]6300)", type=float, default=25)
     parser.add_argument("-l", "--linegroups", nargs='+', default="HeII4686", help="Space-separated groups of comma-separated lines to fit. (e.g. if you want two groups with same continuum: HBETA OIII4959,OIII5007)", type=str) # 
+    parser.add_argument("--cpus", help="Number of CPUs to use for parallel processing (currently not implemented for single spectrum)", type=int, default=1)
     args = parser.parse_args()
 
     outpath = args.outdir
     sigma = args.sigma
     degree = args.degree
-    if args.distance is not None:
-        D = args.distance * 1e6  / CM_TO_PC
 
     if not os.path.isdir(outpath):
         os.mkdir(outpath)
@@ -82,8 +84,8 @@ if __name__ == "__main__":
 
     # Create log file name based on line groups
     linegroupout = "_".join([line for group in args.linegroups for line in group.split(",")])
-    logfile = f"{outpath}/fit_spectrum_{linegroupout}.log"
-    setup_logging(logfile, logging.DEBUG)
+    logfile = f"{outpath}/fit_cube_{linegroupout}.log"
+    setup_logging(logfile, logging.INFO)
     
     inputgroups = args.linegroups#[["HeII4686"]]#[["HBETA", "OIII4959", "OIII5007"], ["OI6300"],
             #["NII6548","HALPHA", "NII6583"], ["SII6716", "SII6731"]]
@@ -105,15 +107,11 @@ if __name__ == "__main__":
         linegroups.append(grouplines)
         # make wavelength range based on the first and last line of the group
         
-        minwav = CATALOG_LINES[grouplines[0]].wave * (1 + redshift) - margin
-        maxwav = CATALOG_LINES[grouplines[-1]].wave * (1 + redshift) + margin
+        minwav = CATALOG_LINES.lines[grouplines[0]].wave * (1 + redshift) - margin
+        maxwav = CATALOG_LINES.lines[grouplines[-1]].wave * (1 + redshift) + margin
         wav_cuts[index] = (minwav, maxwav)
         for line in grouplines:
-            if line in fit_lines.keys():
-                logger.info(f"Adding broad component for {line}")
-                fit_lines[f"{line}_broad"] =  CATALOG_LINES[line]
-            else:
-                fit_lines[line] = CATALOG_LINES[line]
+            fit_lines[line] = CATALOG_LINES.lines[line]
 
 
     # get the first line of the first group and the last line of the last group
@@ -124,8 +122,6 @@ if __name__ == "__main__":
     # cut the cube over the wavelength range we will not needed
     data_spectrum.mask_region(waveinit - step *1.01, waveend + step * 1.01, inside=False)
     data_spectrum.crop()
-    if np.all(data_spectrum.mask):
-        raise SpectrumMaskedError("Spectrum is completely masked! Check the input spectrum, try different lines or wavelenght cuts")
 
     # mask the data spectrum outside the wavelength range of interest, important to copy here
     original_mask = data_spectrum.data.mask.copy()  
@@ -145,26 +141,22 @@ if __name__ == "__main__":
     logger.info(f"Fitting spectrum {specname} with {len(fit_lines)} lines,  {len(linegroups)} line groups")
     
     try:
-        result, conf = fit_spectrum(data_spectrum, fit_lines, redshift=redshift, sigma=sigma, 
-                                    wavelengths=wavelengths, degree=degree, uncertainties=True)
+        result, conf = fit_spectrum(data_spectrum, fit_lines, redshift=redshift, sigma=sigma, wavelengths=wavelengths, linegroups=linegroups, degree=degree, uncertainties=True)
     except (SpectrumMaskedError, DofError) as e:
         logger.error(f"Fitting failed: {e}")
         exit(1)
     save_model(result, f"{outpath}/{linegroupout}_model.sav")
-    print(result.fit_report())
-    if conf is not None:
-        print(ci_report(conf, ndigits=3))
-    if len(fit_lines)==1:
-        fig = plot_fit(wavelengths[~data_spectrum.mask], result, lref=fit_lines[line].wave, z_sys=redshift, annotate=False, normalize=True)
-    else:
-        fig = plot_fit(wavelengths[~data_spectrum.mask], result, z_sys=redshift, annotate=False, normalize=True)
+    print(ci_report(conf, ndigits=3))
+    fig = plot_fit(wavelengths[~data_spectrum.mask], result)
 
-    fig.savefig(f"{outpath}/{linegroupout}_fit")
+    fig.savefig(f"{outpath}/{linegroupout}_fit.png", dpi=200)
+
+    print("VAR ALL CLOSE", np.allclose(result.weights, 1.0 / data_spectrum.var[~data_spectrum.mask]**0.5))
+    print("DATA ALL CLOSE", np.allclose(result.data, data_spectrum.data[~data_spectrum.mask]))
 
     cont_prefix = 'cont'
     noise = np.std(result.data - result.best_fit)
     redchi = result.redchi
-    bic = result.bic
     rsquared = result.rsquared
     ndata = data_spectrum.data.size
     nvarys = result.nvarys
@@ -173,15 +165,11 @@ if __name__ == "__main__":
     param_names = []
     
     # Add basic fit statistics
-    param_names.extend(['redchi',  'bic', 'rsquared', 'ndof', 'ndata', 'ier'])
-    param_values.extend([redchi, bic, rsquared, 
+    param_names.extend(['redchi', 'rsquared', 'ndof', 'ndata', 'ier'])
+    param_values.extend([redchi, rsquared, 
                         ndata - nvarys, ndata, result.ier])
     # Process each line
-    previouslinename = None
     for linename in fit_lines.keys():
-
-        if previouslinename ==linename:
-            linename += "_broad"
         line = fit_lines[linename]
         best_values = result.params
         
@@ -193,7 +181,7 @@ if __name__ == "__main__":
         param_values.append(snr)
         
         # Store basic line parameters
-        linepars = ["amplitude", "vel", "fwhm_vel"]
+        linepars = ["amplitude", "fwhm", "center"]
         for par in linepars:
             param_name = "%s_%s" % (linename, par)
             if param_name in best_values:
@@ -206,64 +194,56 @@ if __name__ == "__main__":
                 # Parameter uncertainty
                 error_value = np.nan
                 if conf is not None:
-                    # handle the broad factor
-                    if par == "fwhm_vel" and "broad" in linename:
-                        confpar = conf["%s_fwhm_factor" %linename]
-                        # multiply sigma of the narrow line with the factor
-                        error_value = get_error(confpar) * best_values["%s_%s" % (linename.replace("_broad", ""), par)]
-                    
+                    # Handle FWHM specially (stored as sigma)
+                    if par == "fwhm":
+                        parname = "%s_sigma" % linename
+                        if parname in conf:
+                            confpar = conf[parname]
+                            error_value = abs(confpar[0][1] - confpar[1][1]) * sigma_to_fwhm
                     elif param_name in conf:
                         confpar = conf[param_name]
-                        error_value = get_error(confpar)
+                        error_value = abs(confpar[0][1] - confpar[1][1])
+                    # store for later
+                    fwhm_value = param.value
+                    efwhm_value = error_value
 
                 elif param.stderr is not None:
                     error_value = param.stderr
-
+                if par=="center":
+                    # store for later
+                    ewavelength = error_value
+                    wavelength = param.value
                 param_names.append(f"{param_name}_err")
                 param_values.append(error_value)
+        
+        # FWHM corrected and FWHM velocity
+        FWHM_inst, eFWHM_inst = get_instrument_FWHM(wavelength, ewavelength)
+        if fwhm_value > FWHM_inst:
+            fwhm_corrected = np.sqrt(fwhm_value**2 - FWHM_inst**2)
+            efwhm_corrected = np.sqrt((fwhm_value * efwhm_value)**2 + (FWHM_inst * eFWHM_inst)**2) / fwhm_corrected
+            fwhm_vel = fwhm_corrected / wavelength * ckms
+            efwhm_vel = np.sqrt(efwhm_corrected**2 + (fwhm_corrected * ewavelength / wavelength)**2) * ckms / wavelength
+        # unresolved line
+        else:
+            fwhm_corrected = 0
+            efwhm_corrected = np.nan
+            fwhm_vel = 0
+            efwhm_vel = np.nan
 
-                if par=="vel":
-                    # store for later
-                    velocity = param.value
-                    evelocity = error_value
-                    wavelength, ewavelength = compute_shift(line.wave, z_sys=redshift, velocity=velocity, evelocity=evelocity)
-                    param_names.append(f"{linename}_center")
-                    param_values.append(wavelength)
-                
-                    param_names.append(f"{linename}_center_err")
-                    param_values.append(ewavelength)
 
-                if par=="amplitude" and args.distance is not None:
-                    param_names.append("%s_lum" % linename)
-                    param_values.append(param.value * 4 * pi * D**2)
-                    param_names.append("%s_lum_err" % linename)
-                    param_values.append(error_value * 4 * pi * D**2)
+        param_names.extend([f"{linename}_fwhm_corrected", f"{linename}_fwhm_corrected_err", f"{linename}_fwhm_vel", f"{linename}_fwhm_vel_err"])
+        param_values.extend([fwhm_corrected, efwhm_corrected, fwhm_vel, efwhm_vel])
 
-                if par == "fwhm_vel":
-                    # FWHM corrected and FWHM velocity
-                    FWHM_inst, eFWHM_inst = get_instrument_FWHM(wavelength, ewavelength)
-                    FWHM_instvel = FWHM_inst / wavelength * ckms
-                    if param.value > FWHM_instvel:
-                        eFWHM_instvel =  ((eFWHM_inst / wavelength)**2. + (FWHM_inst * ewavelength / wavelength**2))**0.5 * ckms
-                        fwhm_vel_corrected, efwhm_vel_corrected = correct_FWHM(param.value, FWHM_instvel, error_value, eFWHM_instvel)
-
-                        # f = A**2 - B **2
-                        # df/dA = 2A
-                        # df/dB = -2B
-                        # df = sqrt( (df/da dA)**2 + (-2B dB)**2)
-                        # df = sqrt( (2A dA)**2 + (2B dB)**2) = 2 sqrt(A dA + B dB)
-                        # F = sqrt(f) = sqrt(A**2 - B**2)
-                        # d sqrt(f) = 1/(2 * sqrt(f))  * df = 1 / (2 F) * df =   sqrt( (A dA)**2 + (B dB)**2) / F
-                    else:
-                        fwhm_vel_corrected = 0
-                        efwhm_vel_corrected = np.nan
-                    
-                    param_names.append(f"{linename}_fwhm_vel_corr")
-                    param_values.append(fwhm_vel_corrected)
-                    param_names.append(f"{linename}_fwhm_vel_corr_err")
-                    param_values.append(efwhm_vel_corrected)
-
-        previouslinename = linename
+        # Velocity
+        if "sky" in line.name:
+            z_sys = 0.0
+        else:
+            z_sys = redshift
+        velocity, evelocity = peculiar_velocity(line.wave, wavelength, 
+                                              ewavelength=ewavelength, z_sys=z_sys)
+        
+        param_names.extend([f"{linename}_vel", f"{linename}_vel_err"])
+        param_values.extend([velocity, evelocity])
     
     # Add continuum parameters
     for i in range(degree + 1):
@@ -275,7 +255,7 @@ if __name__ == "__main__":
         error_value = np.nan
         if conf is not None and f"{cont_prefix}_c{i}" in conf:
             confpar = conf[f"{cont_prefix}_c{i}"]
-            error_value = get_error(confpar)
+            error_value = np.abs(confpar[0][1] - confpar[1][1])
         elif param.stderr is not None:
             error_value = param.stderr
         
